a8796678764fbb95e2b4986890977b581bfe7ad5
Heidi Zhang <qingyuanhzhang@gmail.com>
2021-08-09 23:18:35 -0700

    very basic google login page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import GoogleLogin from 'react-google-login';
 import './App.css';
 
 function App() {
+
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+
+  const responseGoogle = (response) => {
+    setName(response.profileObj.name)
+    setEmail(response.profileObj.email)
+    console.log(email)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Login With Google</h1>
+      <h2>Welcome {name}</h2>
+      <GoogleLogin
+    clientId="8883512831-jh64k5os3e6len7ij617j3k6r6vk3ms3.apps.googleusercontent.com"
+    buttonText="Login"
+    onSuccess={responseGoogle}
+    onFailure={responseGoogle}
+    cookiePolicy={'single_host_origin'}
+  />
     </div>
   );
 }
