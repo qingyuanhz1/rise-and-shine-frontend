0dbbd80dbe5112d89f8a9929963b19a0d427ce29
Heidi Zhang <qingyuanhzhang@gmail.com>
2021-08-12 21:22:02 -0700

    Calendar showing start time to end time for recurring events, conditional rendering Calendar when Calendar Button is clicked

--- src/components/EventCalendar.js
+++ src/components/EventCalendar.js
@@ -3,6 +3,8 @@ import FullCalendar from '@fullcalendar/react' // must go before plugins
 import rrulePlugin from '@fullcalendar/rrule'
 import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
 import moment from 'moment';
+import momentDurationFormatSetup from 'moment-duration-format';
+momentDurationFormatSetup(moment);
 
 const EventCalendar = (props) => {
     
@@ -16,24 +18,31 @@ const EventCalendar = (props) => {
         } else {
             let recurringEvent = {} 
             recurringEvent.title = event.title
-            const m = moment(event.start)
-            console.log(m)
-            recurringEvent.startTime = m.format('YYYYMMDD[T]HHmmss')
-            console.log(recurringEvent.startTime)
-            recurringEvent.endTime = event.end
+            const mStart = moment(event.start)
+            recurringEvent.startTime = mStart.format('YYYYMMDD[T]HHmmss')
+            const mEnd = moment(event.end)
+            const duration = moment.duration(mEnd.diff(mStart)).as('minutes')
+            const reformatDuration = moment.duration(duration,'minutes').format("hh:mm", {trim:false})
+            recurringEvent.duration = reformatDuration
             recurringEvent.rrule = `DTSTART:${recurringEvent.startTime}\n${event.recurrence.replace('Z;', ';')}`
-            console.log(recurringEvent.rrule)
             organizedEvents.push(recurringEvent)
         }
     }
-
-    return (
-    <FullCalendar
-    plugins={[ dayGridPlugin,  rrulePlugin]}
-    initialView="dayGridMonth"
-    events={organizedEvents}
-    />
-    )
+    
+    if (organizedEvents.length === 0) {
+        return <div/>
+    } else {
+        return (
+            <FullCalendar
+            plugins={[ dayGridPlugin,  rrulePlugin]}
+            initialView="dayGridMonth"
+            showNonCurrentDates={false}
+            displayEventEnd={true}
+            displayEventTime={true}
+            events={organizedEvents}
+            />
+            )
+    }
     // return (
     //     <FullCalendar
     //     plugins={[ dayGridPlugin,  rrulePlugin]}
@@ -61,15 +70,17 @@ const EventCalendar = (props) => {
     //     <FullCalendar
     //     plugins={[ dayGridPlugin, rrulePlugin]}
     //     initialView="dayGridMonth"
+    //     displayEventEnd="true"
+    //     displayEventTime="true"
     //     events={[
     //         {
     //             title: 'my recurring event 1',
-    //             // startTime: '10:30:000',
-    //             // endTime: '11:30:000',
+    //             duration: "01:00",
     //             rrule: 'DTSTART:20210810T103000\nRRULE:FREQ=WEEKLY;UNTIL=20210910;BYDAY=TU,TH'
     //         },
     //         {
     //             title: 'my recurring event 2',
+    //             duration: "00:20",
     //             rrule: 'DTSTART:20210727T110000\nRRULE:FREQ=WEEKLY;WKST=SU;UNTIL=20210819;BYDAY=MO,TH,TU,WE'
     //         },
     //         { title: 'event 3', date: '2021-08-12' },
